6431aaedf11e9d68ad40fd82deb35946233149d7
ysdy44 <ysdy44@outlook.com>
2023-05-29 00:37:23 +0800

    添加项目文件。

---
+++ Code Graph/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Code_Graph
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
