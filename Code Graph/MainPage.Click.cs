85ad2a240ef3ec0065bf00a398576d8cd635be06
ysdy44 <ysdy44@outlook.com>
2023-05-29 01:08:13 +0800

    Add MainPage.Click and Drag

---
+++ Code Graph/MainPage.Click.cs
@@ -0,0 +1,200 @@
+﻿using Code_Graph.Project;
+using Code_Graph.Project.Datas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Shapes;
+
+namespace Code_Graph
+{
+    partial class MainPage
+    {
+        public async void Click(OptionType type)
+        {
+            switch (type)
+            {
+                case OptionType.Update:
+                    {
+                        for (int i = 0; i < this.Links.Length; i++)
+                        {
+                            KeyValuePair<int, int> link = this.Links[i];
+                            Group key = this.Groups[link.Key];
+                            Group value = this.Groups[link.Value];
+
+                            Line line = this.LineChildren[i] as Line;
+                            line.X1 = key.X * 10;
+                            line.Y1 = key.Y * 10;
+                            line.X2 = value.X * 10;
+                            line.Y2 = value.Y * 10;
+
+                            Ellipse ellipse = this.EllipseChildren[i] as Ellipse;
+                            Thumb thumb = this[link.Value];
+                            double w = thumb.ActualWidth;
+                            double h = thumb.ActualHeight;
+
+                            Point arrow = new IntersectsRectAndLine(w, h, value.X * 10, value.Y * 10, key.X * 10, key.Y * 10).Arrow;
+                            Canvas.SetLeft(ellipse, arrow.X - 6);
+                            Canvas.SetTop(ellipse, arrow.Y - 6);
+                        }
+                    }
+                    break;
+
+                case OptionType.Clear:
+                    {
+                        foreach (Thumb thumb in this.ThumbChildren)
+                        {
+                            thumb.SizeChanged -= this.DragSizeChanged;
+                            thumb.DragStarted -= this.DragStarted;
+                            thumb.DragDelta -= this.DragDelta;
+                            thumb.DragCompleted -= this.DragCompleted;
+                        }
+                        this.LineChildren.Clear();
+                        this.EllipseChildren.Clear();
+                        this.ThumbChildren.Clear();
+                    }
+                    break;
+                case OptionType.Add:
+                    {
+                        for (int i = 0; i < this.Links.Length; i++)
+                        {
+                            this.LineChildren.Add(new Line());
+                            this.EllipseChildren.Add(new Ellipse());
+                        }
+
+                        foreach (Group item in this.Groups)
+                        {
+                            if (item.Level == default) continue;
+
+                            Thumb thumb = new Thumb
+                            {
+                                Tag = this.Files.DisplayName(item.Source),
+                                Style = this[item.Level]
+                            };
+                            thumb.SizeChanged += this.DragSizeChanged;
+                            thumb.DragStarted += this.DragStarted;
+                            thumb.DragDelta += this.DragDelta;
+                            thumb.DragCompleted += this.DragCompleted;
+                            this.ThumbChildren.Add(thumb);
+                        }
+                    }
+                    break;
+
+                case OptionType.New:
+                    {
+                        // FolderPicker
+                        FolderPicker folderPicker = new FolderPicker
+                        {
+                            SuggestedStartLocation = default,
+                            FileTypeFilter =
+                            {
+                                "*"
+                            }
+                        };
+
+                        // PickSingleFolderAsync
+                        StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+                        if (folder is null) return;
+
+                        IList<CsprojLine> lines = new List<CsprojLine>();
+                        foreach (StorageFolder item in await folder.GetFoldersAsync())
+                        {
+                            foreach (StorageFile file in await item.GetFilesAsync())
+                            {
+                                if (file.FileType == ProjectsExtensions.FileType)
+                                {
+                                    IList<string> lines2 = await FileIO.ReadLinesAsync(file);
+                                    lines.Add(new CsprojLine
+                                    {
+                                        Name = file.Name,
+                                        DisplayName = file.DisplayName,
+                                        Lines = lines2.Where(ProjectsExtensions.Contains).ToArray()
+                                    });
+                                }
+                            }
+                        }
+
+                        this.Click(OptionType.Clear);
+                        if (lines.Count == 0) break;
+
+                        this.Files = lines.ToFiles();
+                        this.Groups = this.Files.ToGroups((int)(base.ActualWidth / 2 / 10), (int)(base.ActualHeight / 2 / 10)).ToArray();
+                        this.Links = this.Groups.ToLines(this.Files).ToArray();
+                        this.Click(OptionType.Add);
+
+                        this.Click(OptionType.Update);
+                    }
+                    break;
+                case OptionType.Save:
+                    {
+                        string suggestedFileName = "Untitled";
+
+                        // FileSavePicker
+                        FileSavePicker savePicker = new FileSavePicker
+                        {
+                            SuggestedStartLocation = default,
+                            SuggestedFileName = suggestedFileName,
+                            FileTypeChoices =
+                            {
+                                {"DB", new[] { ProjectsExtensions.FileChoices } }
+                            }
+                        };
+
+                        // PickSaveFileAsync
+                        StorageFile file = await savePicker.PickSaveFileAsync();
+                        if (file is null) break;
+
+                        using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                        {
+                            new XDocument
+                            (
+                                new XElement("Root", this.Groups.Select(this.Save))
+                            ).Save(stream.AsStream());
+                        }
+                    }
+                    break;
+                case OptionType.Load:
+                    {
+                        // FileOpenPicker
+                        FileOpenPicker openPicker = new FileOpenPicker
+                        {
+                            SuggestedStartLocation = default,
+                            FileTypeFilter =
+                            {
+                                { ProjectsExtensions.FileChoices }
+                            }
+                        };
+
+                        // PickSingleFileAsync
+                        StorageFile file = await openPicker.PickSingleFileAsync();
+                        if (file is null) break;
+
+                        this.Click(OptionType.Clear);
+                        using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+                        {
+                            XDocument document = XDocument.Load(stream.AsStream());
+                            IEnumerable<GroupData> datas = this.Load(document);
+                            if (datas.Count() == 0) break;
+
+                            this.Files = datas.ToFiles();
+                            this.Groups = this.Files.ToGroups(datas).ToArray();
+                            this.Links = this.Groups.ToLines(this.Files).ToArray();
+                            this.Click(OptionType.Add);
+
+                            this.Click(OptionType.Update);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Code Graph/MainPage.Drag.cs
@@ -0,0 +1,102 @@
+﻿using Code_Graph.Project;
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Shapes;
+
+namespace Code_Graph
+{
+    partial class MainPage
+    {
+        private void DragSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize == Size.Empty) return;
+            if (e.NewSize == e.PreviousSize) return;
+
+            if (sender is FrameworkElement item)
+            {
+                int index = this.ThumbChildren.IndexOf(item);
+                if (index < 0) return;
+
+                Group group = this.Groups[index];
+                if (group.X == default && group.Y == default) return;
+
+                double w = e.NewSize.Width;
+                double h = e.NewSize.Height;
+
+                Canvas.SetLeft(item, group.X * 10 - (w / 2));
+                Canvas.SetTop(item, group.Y * 10 - (h / 2));
+
+                for (int i = 0; i < this.Links.Length; i++)
+                {
+                    KeyValuePair<int, int> link = this.Links[i];
+
+                    if (link.Key == index)
+                    {
+                        Line line = this.LineChildren[i] as Line;
+                        line.X1 = group.X * 10;
+                        line.Y1 = group.Y * 10;
+                    }
+
+                    if (link.Value == index)
+                    {
+                        Line line = this.LineChildren[i] as Line;
+                        line.X2 = group.X * 10;
+                        line.Y2 = group.Y * 10;
+
+                        Group key = this.Groups[link.Key];
+                        Group value = group;
+                        Point arrow = new IntersectsRectAndLine(w, h, value.X * 10, value.Y * 10, key.X * 10, key.Y * 10).Arrow;
+
+                        Ellipse ellipse = this.EllipseChildren[i] as Ellipse;
+                        Canvas.SetLeft(ellipse, arrow.X - 6);
+                        Canvas.SetTop(ellipse, arrow.Y - 6);
+                    }
+                }
+            }
+        }
+        private void DragStarted(object sender, DragStartedEventArgs e)
+        {
+            if (sender is FrameworkElement item)
+            {
+                this.StartingIndex = this.ThumbChildren.IndexOf(item);
+                if (this.StartingIndex < 0) return;
+
+                Group group = this.Groups[this.StartingIndex];
+                this.StartingPoint.X = group.X * 10;
+                this.StartingPoint.Y = group.Y * 10;
+            }
+        }
+        private void DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (this.StartingIndex < 0) return;
+
+            this.StartingPoint.X += e.HorizontalChange;
+            this.StartingPoint.Y += e.VerticalChange;
+
+            if (sender is FrameworkElement item)
+            {
+                int x = (int)(this.StartingPoint.X / 10);
+                int y = (int)(this.StartingPoint.Y / 10);
+
+                Group group = this.Groups[this.StartingIndex];
+                if (group.X == x && group.Y == y) return;
+
+                group.X = x;
+                group.Y = y;
+
+                Canvas.SetLeft(item, x * 10 - (item.ActualWidth / 2));
+                Canvas.SetTop(item, y * 10 - (item.ActualHeight / 2));
+            }
+            this.Click(OptionType.Update);
+        }
+        private void DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (this.StartingIndex < 0) return;
+
+            this.Click(OptionType.Update);
+        }
+    }
+}
