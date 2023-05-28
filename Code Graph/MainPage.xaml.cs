2d088eb3a7a50080016deeb02c5049613f03e5de
ysdy44 <ysdy44@outlook.com>
2023-05-29 01:06:42 +0800

    Add MainPage UI

--- Code Graph/MainPage.xaml.cs
+++ Code Graph/MainPage.xaml.cs
@@ -16,9 +16,29 @@ namespace Code_Graph
         int StartingIndex;
         Point StartingPoint;
 
+        Style this[Level item] => this.Resources[$"{item}Style"] as Style;
+        Thumb this[int item] => this.ThumbCanvas.Children[item] as Thumb;
+
+        UIElementCollection LineChildren => this.LineCanvas.Children;
+        UIElementCollection EllipseChildren => this.EllipseCanvas.Children;
+        UIElementCollection ThumbChildren => this.ThumbCanvas.Children;
+
         public MainPage()
         {
             this.InitializeComponent();
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+
+                this.AlignmentGrid.RebuildWithInterpolation(e.NewSize);
+            };
+            this.AppBarListView.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is SymbolIcon item)
+                {
+                }
+            };
         }
     }
 }
