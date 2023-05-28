e84995eafffd922712e34549b7aece0acea3a3d3
ysdy44 <ysdy44@outlook.com>
2023-05-29 00:49:56 +0800

    Add Group

---
+++ Code Graph.Project/Group.cs
@@ -0,0 +1,11 @@
+﻿namespace Code_Graph.Project
+{
+    public sealed class Group
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Level Level { get; internal set; }
+        public int[] Source { get; set; }
+        public override string ToString() => this.Source == null ? $"{this.Level}" : $"{this.Level} {this.Source.Length}";
+    }
+}
