75f609baec781e95424b04abcdee6d16f16e179b
ysdy44 <ysdy44@outlook.com>
2023-05-30 23:52:52 +0800

    Add AboutDialog

---
+++ Code Graph/AboutDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Code_Graph
+{
+    public sealed partial class AboutDialog : ContentDialog
+    {
+        //@Strings
+        private Uri GithubLink => new Uri(App.Resource.GetString(UIType.GithubLink.ToString()));
+        private Uri FeedbackLink => new Uri(App.Resource.GetString(UIType.FeedbackLink.ToString()));
+
+        //@Construct
+        public AboutDialog()
+        {
+            this.InitializeComponent();
+            this.AboutImage.DoubleTapped += (s, e) => this.AboutStoryboard.Begin(); // Storyboard
+
+            base.SecondaryButtonClick += (s, e) => base.Hide();
+            base.PrimaryButtonClick += (s, e) => base.Hide();
+        }
+    }
+}
