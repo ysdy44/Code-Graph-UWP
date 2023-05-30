556b023afc41418cf77dc2b9438c9de133214fc3
ysdy44 <ysdy44@outlook.com>
2023-05-30 23:53:54 +0800

    Add TutorialPage

---
+++ Code Graph/TutorialPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Code_Graph
+{
+    public sealed partial class TutorialPage : Page
+    {
+        public TutorialPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
