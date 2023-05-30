9c35ddb85eed3271d3208da16b25becd7d1c3c9a
ysdy44 <ysdy44@outlook.com>
2023-05-30 23:54:58 +0800

    Add ExpandStackPanel

---
+++ Code Graph.Elements/ExpandStackPanel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace Code_Graph.Elements
+{
+    public sealed partial class ExpandStackPanel : StackPanel
+    {
+        public ExpandStackPanel(IEnumerable<string> text)
+        {
+            this.InitializeComponent();
+            foreach (var item in text)
+            {
+                base.Children.Add(new TextBlock
+                {
+                    Text = item
+                });
+            }
+        }
+    }
+}
