8fdd5d1bf5920558cb9796155260f44511c74fd3
ysdy44 <ysdy44@outlook.com>
2023-05-30 23:45:46 +0800

    Add UIType

---
+++ Code Graph/Enums/UIType.cs
@@ -0,0 +1,30 @@
+﻿namespace Code_Graph
+{
+    public enum UIType
+    {
+        Name,
+
+        Version,
+        Github,
+        Feedback,
+
+
+        VersionTitle,
+        GithubTitle,
+        FeedbackTitle,
+
+        GithubLink,
+        FeedbackLink,
+
+
+        Cancel,
+        OK,
+
+        New,
+        Open,
+        Save,
+        Clear,
+        Tutorial,
+        About,
+    }
+}
