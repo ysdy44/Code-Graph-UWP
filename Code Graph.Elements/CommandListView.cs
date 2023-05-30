754e343dbdee1443d6e57e66d769d13488c68415
ysdy44 <ysdy44@outlook.com>
2023-05-30 23:50:10 +0800

    Add CommandListView

---
+++ Code Graph.Elements/CommandListView.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Code_Graph.Elements
+{
+    public sealed class CommandListView : ListView
+    {
+        //@Command
+        public ICommand Command { get; set; }
+        public CommandListView()
+        {
+            base.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is SymbolIcon item)
+                {
+                    this.Command?.Execute(item.Symbol); // Command
+                }
+            };
+        }
+    }
+}
