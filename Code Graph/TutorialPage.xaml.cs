4835f751435eb15fd53fdd07dbe659d15a624fbf
ysdy44 <ysdy44@outlook.com>
2023-06-02 21:11:57 +0800

    Add TutorialPage

--- Code Graph/TutorialPage.xaml.cs
+++ Code Graph/TutorialPage.xaml.cs
@@ -7,6 +7,18 @@ namespace Code_Graph
         public TutorialPage()
         {
             this.InitializeComponent();
+
+            this.Forward00Button.Click += (s, e) => this.FlipView.SelectedIndex = 1;
+            this.Forward02Button.Click += (s, e) => this.FlipView.SelectedIndex = 2;
+            this.Forward03Button.Click += (s, e) => this.FlipView.SelectedIndex = 3;
+
+            this.Button.Click += (s, e) =>
+            {
+                if (base.Frame.CanGoBack)
+                    base.Frame.GoBack();
+                else
+                    base.Frame.Navigate(typeof(MainPage));
+            };
         }
     }
 }
